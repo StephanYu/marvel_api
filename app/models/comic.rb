651101ac7ec7ba7ab8a5040b52b65b8525014030
Stephan Yue <syue@Stephans-Mac-mini.local>
2016-08-15 18:23:36 +0100

    add some comments to comic model regarding SQL queries for HABTM relationship between comics and characters

--- app/models/comic.rb
+++ app/models/comic.rb
@@ -4,4 +4,10 @@ class Comic < ApplicationRecord
   def self.search(search_term)
     where("title LIKE ?", "%#{search_term}%")
   end
+
+  # comics = Comic.joins("join characters_comics on comic.id = characters_comics.character_id").where(["characters_comics.character_id = ?", character_id])
+
+  # @comics = Comic.includes(:characters).where(characters: { name: search_term })
+  # @comics = Comic.includes(:characters).where('character.name' => search_term)
+
 end
