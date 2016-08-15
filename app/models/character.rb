c01a2e262d1c695ab6c8d8ed5819297445637ae2
Stephan Yu <Stephan.yu@gmail.com>
2016-08-15 06:00:29 +0100

    refactor import service

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,7 +1,3 @@
 class Character < ApplicationRecord
   has_and_belongs_to_many :comics
-
-  def self.search(search)
-    where("name LIKE ?", "%#{search}%").includes(comics) 
-  end
 end
--- app/models/comic.rb
+++ app/models/comic.rb
@@ -2,7 +2,7 @@ class Comic < ApplicationRecord
   has_and_belongs_to_many :characters
 
   def self.search(search)
-    Character.search(search)
     where("title LIKE ?", "%#{search}%")
+    where("characters LIKE ?", "%#{search}%")
   end
 end
